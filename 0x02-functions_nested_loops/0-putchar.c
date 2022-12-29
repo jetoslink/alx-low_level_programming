9a6a0e9cdca29d85366b4c16b0bd87d9dac02770
ogundele <jetoslink@gmail.com>
2022-12-28 22:35:07 -0800

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -10,14 +10,14 @@
 
 int main(void)
 {
-	putchar(');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
+	int i = 0;
+	char text[] = "_putchar";
+
+	while (text[i] != '\0')
+	{
+		_putchar(text[i]);
+		i++;
+	}
 	_putchar('\n');
 
 	return (0);
