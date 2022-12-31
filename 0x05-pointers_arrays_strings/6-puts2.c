d5cc08c277190cb4a2ae2057eaa0af4b342fd4cb
ogundele <jetoslink@gmail.com>
2022-12-31 07:29:35 -0800

    print string

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -5,18 +5,22 @@
  * puts2 - puts string twice
  *
  * @str: string to put twice
+ * Return: no return
  */
 
 void puts2(char *str)
 {
-	int i;
+	int count = 0;
 
-	for (i = 0; i < _strlen(str); i++)
+	while (count >= 0)
 	{
-		if (i % 2 == 0)
+		if (str[count] == '\0')
 		{
-			_putchar(str[i]);
+			_putchar('\n');
+			break;
 		}
+		if (count % 2 == 0)
+			_putchar(str[count]);
+		count++;
 	}
-	_putchar('\n');
 }
